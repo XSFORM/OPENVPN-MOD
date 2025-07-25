f6363c397f48d643ced2266ae90db7a9913fbcae
XSFORM <uk.datacloud.com@gmail.com>
2025-07-25 20:53:24 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-apt-get update
+max_attempts=20
+attempt=0
+
+while [ $attempt -lt $max_attempts ]; do
+    if apt-get update; then
+        break
+    else
+        echo "apt-get update failed due to lock, waiting 5 seconds..."
+        sleep 5
+        attempt=$((attempt+1))
+    fi
+done
+
 apt-get -y install python3-pip curl sqlite3
 curl -L https://github.com/XSFORM/OPENVPN-MOD/raw/main/assets/openvpn.tar.gz -o /tmp/openvpn.tar.gz
 tar -xzf /tmp/openvpn.tar.gz -C /usr/lib
